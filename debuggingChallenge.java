5c6845c0f1fa04d0598256d1583e78ead00afb66
Brendan Lucas <brendanwflucas@gmail.com>
2017-03-18 01:43:44 -0400

    Create debuggingChallenge.java

---
+++ debuggingChallenge.java
@@ -0,0 +1,13 @@
+
+public class debuggingChallenge{ 
+	
+	public static void main(String[] args){
+		int[] arr = {1,2,3,4,5,6,7,8,9,10,11};
+		int[] reverse = new int[arr.length];
+		for(int i= 0; i<=arr.length; i++){
+			reverse[arr.length-i] = arr[i];
+		}
+		System.out.println(reverse);
+	}
+	
+}
